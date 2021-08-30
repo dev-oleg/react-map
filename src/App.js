8198836b70a8ab85f699e2e126824ca6bbe626c3
oleg <olegvolobuev886@gmail.com>
2021-08-30 22:50:02 +0300

    базовая разметка

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
+import './App.css'
+import Map from './components/Map/Map'
+import Widget from './components/Widget/Widget'
 
 class App extends Component {
+    state = {
+        inputText: '',
+        inputValid: false,
+        showErrorMessage: false
+    }
+
     render() {
         return (
-            <div>
-                Hello World
+            <div className = 'App'>
+                <Map />
+
+                <Widget />
             </div>
         )
     }
---
+++ src/components/Widget/Widget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Widget.css'
+
+const Widget = props => {
+    return (
+        <div className = 'Widget'></div>
+    )
+}
+
+export default Widget
