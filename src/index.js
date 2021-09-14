5b5fe3330b3c63e261834f292bb1bd341d219daf
oleg <olegvolobuev886@gmail.com>
2021-09-14 22:58:19 +0300

    установлены библиотеки redux и react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createStore, compose, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css'
 import App from './App'
+import rootReducer from './redux/rootReducer'
+
+const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) :
+        compose
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
+
+const app = <Provider store = {store}>
+    <App />
+</Provider>
 
 ReactDOM.render(
-    <App />,
+    app,
     document.getElementById('root')
 )
