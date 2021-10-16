a252c348cece8241f4bccc05f58f5f3a51de9676
oleg <olegvolobuev886@gmail.com>
2021-10-16 18:30:35 +0300

    удалены лишние файлы

--- src/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const FETCH_START = 'FETCH_START'
-export const FETCH_FINISH = 'FETCH_FINISH'
-
-export const SET_MAP_ELEMENT = 'SET_MAP_ELEMENT'
-
-export const CLEAR_STATE = 'CLEAR_STATE'
-
-export const SET_MAP = 'SET_MAP'
